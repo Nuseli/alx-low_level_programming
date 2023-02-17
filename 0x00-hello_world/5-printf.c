87f835fbdaa8f124b37b147ae4866d310f7dba6a
Nuseli <matthewaglonu2131@gmail.com>
2023-02-16 16:14:19 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,12 +3,12 @@
 /**
  * main - prints "with proper grammar, but the outcome is
  * a piece of art," followed newline
- * Return: 0.
+ * Return: Always 0.
  *
  */
 
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n")
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
